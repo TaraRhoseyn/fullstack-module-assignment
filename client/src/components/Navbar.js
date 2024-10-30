9841d42b6248cf9d4cf2bb125084d2837a1a3759
Tara Rhoseyn <taragulwell@gmail.com>
2024-10-30 18:50:14 +0100

    beautify navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+
 function Navbar() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const navigate = useNavigate();
@@ -20,32 +21,46 @@ function Navbar() {
     return (
         <nav className="d-flex flex-column p-5 mr-5">
             <Link to="/">
-                <p>Home</p>
+				<p className="text-white text-hover-primary">
+					<i className="bi bi-house-door-fill me-2"></i> Home
+				</p>
             </Link>
             {isLoggedIn ? (
                 <>
                     <Link to="/add">
-                        <p>Add</p>
+						<p className="text-white text-hover-primary">
+							<i className="bi bi-plus-lg me-2"></i> Add
+						</p>
                     </Link>
                     <Link to="/view">
-                        <p>View</p>
+						<p className="text-white text-hover-primary">
+							<i className="bi bi-eye me-2"></i> View
+						</p>
                     </Link>
                     <Link to="/" onClick={handleLogout}>
-                        <p>Logout</p>
+						<p className="text-white text-hover-primary">
+							<i className="bi bi-box-arrow-in-left me-2"></i> Log out
+						</p>
                     </Link>
                 </>
             ) : (
                 <>
                     <Link to="/login">
-                        <p>Log In</p>
+						<p className="text-white text-hover-primary">
+							<i className="bi bi-box-arrow-in-right me-2"></i> Log in
+						</p>
                     </Link>
                     <Link to="/signup">
-                        <p>Sign Up</p>
+						<p className="text-white text-hover-primary">
+							<i className="bi bi-pencil-square me-2"></i> Sign up
+						</p>
                     </Link>
                 </>
             )}
             <Link to="/about">
-                <p>About</p>
+				<p className="text-white text-hover-primary">
+					<i className="bi bi-file-person-fill me-2"></i> About
+				</p>
             </Link>
         </nav>
     );
